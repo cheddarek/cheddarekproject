9af56282c7182bf777733dfef1800083f22f9110
cheddarek <63371786+cheddarek@users.noreply.github.com>
2020-04-09 20:16:08 +0200

    User + Model ( Containing User Class Added)

---
+++ Model.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from marshmallow import Schema, fields, pre_load, validate
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+
+ma = Marshmallow()
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(150), unique=False, nullable=False)
+    prenom = db.Column(db.String(150), unique=False, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    phone =  db.Column(db.String(8), unique=False, nullable=False)
+    pwd =  db.Column(db.String(150), unique=False, nullable=False)
+    municipality =  db.Column(db.String(150), unique=False, nullable=False)
+    lieu =  db.Column(db.String(150), unique=False, nullable=False)
+    def __init__(self, email , nom , prenom , phone , pwd , municipality , lieu):
+        self.email = email
+        self.nom = nom
+        self.prenom = prenom
+        self.phone = phone
+        self.pwd = pwd
+        self.municipality = municipality
+        self.lieu = lieu
+
+
+class UserSchema(ma.Schema):
+    id = fields.Integer()
+    nom = fields.String(required=True)
+    prenom = fields.String(required=True)
+    email = fields.String(required=True)
+    phone = fields.String(required=True)
+    pwd = fields.String(required=True)
+    municipality = fields.String(required=True)
+    lieu = fields.String(required=True)
---
+++ User.py
@@ -0,0 +1,68 @@
+from flask import request
+from flask_restful import Resource
+from Model import db, User, UserSchema
+
+users_schema = UserSchema(many=True)
+user_schema = UserSchema()
+
+class UserResource(Resource):
+    def get(self):
+        users = User.query.all()
+        users = users_schema.dump(users)
+        return {'status': 'success', 'data': users}, 200
+
+    def post(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data= user_schema.load(json_data)
+        user = User.query.filter_by(email=data['email']).first()
+
+
+        if user:
+            return {'message': 'User already exists'}, 400
+        user = User(
+            email=json_data['email']
+            ,nom=json_data['nom'],
+            prenom=json_data['prenom'],
+            phone=json_data['phone'],
+            pwd=json_data['pwd'],
+            municipality=json_data['municipality'],
+            lieu=json_data['lieu'],
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        result = user_schema.dump(user)
+
+        return {"status": 'success', 'data': result}, 201
+
+    def put(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data = user_schema.load(json_data)
+
+        category = User.query.filter_by(id=data['id']).first()
+        if not category:
+            return {'message': 'Category does not exist'}, 400
+        category.name = data['name']
+        db.session.commit()
+
+        result = user_schema.dump(category)
+
+        return {"status": 'success', 'data': result}, 204
+
+    def delete(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data = user_schema.load(json_data)
+        user = User.query.filter_by(id=data['id']).delete()
+        db.session.commit()
+        result = user_schema.dump(user)
+        return {"status": 'success', 'data': result}, 204
